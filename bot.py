d653e50530aff91f16cd107e329823a9acaaec88
far240ali <far240ali@gmail.com>
2025-06-05 16:03:45 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 from pyrogram import Client, filters
-from config import API_ID, API_HASH, BOT_TOKEN
+#from config import API_ID, API_HASH, BOT_TOKEN
 from instagram import download_instagram_post
+
+API_ID = '3335796'
+API_HASH = '138b992a0e672e8346d8439c3f42ea78'
+BOT_TOKEN = '1396293494:AAE6YAY-Vog3QPvSNCo8x80FsIue9FJGWh8'
 
 bot = Client("insta-downloader-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
