78531ee4cbd91000d0742299c6f771a996914481
far240ali <far240ali@gmail.com>
2025-06-05 16:23:21 +0330

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,27 +1,30 @@
 import requests
-import re
 
-def download_instagram_post(url):
-    session = requests.Session()
+def download_instagram_post(insta_url):
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
     try:
-        res = session.post("https://igram.io/api/ajaxSearch", headers=headers, data={"q": url})
+        res = requests.post(
+            "https://igram.io/api/ajaxSearch",
+            headers=headers,
+            data={"q": insta_url}
+        )
+
         if res.status_code != 200:
             return None
 
         data = res.json()
+        media_list = []
 
-        results = []
         for item in data.get("medias", []):
             media_url = item.get("url")
             media_type = "video" if ".mp4" in media_url else "image"
-            results.append({"type": media_type, "url": media_url})
+            media_list.append({"type": media_type, "url": media_url})
 
-        return results
+        return media_list if media_list else None
 
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"[ERROR] {e}")
         return None
